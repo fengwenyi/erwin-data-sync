1362f4ff98aa6d6569778c2b63d323a66fed439a
ErwinFeng <xfsy_2015@163.com>
2023-04-24 17:59:31 +0800

    project init

---
+++ src/main/java/com/fengwenyi/erwin/datasync/ErwinDataSyncApplication.java
@@ -0,0 +1,13 @@
+package com.fengwenyi.erwin.datasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErwinDataSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErwinDataSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fengwenyi/erwin/datasync/ErwinDataSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fengwenyi.erwin.datasync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErwinDataSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
